447a665ce0f862f359975cf8f1d946710e3cedcc
gbuomprisco <giancarlopsk@gmail.com>
2014-06-13 10:58:48 +0100

    Changed files structure, changed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 setup(name='Digital Lightbox',
       version='0.1',
       description='Digital Lightbox for Manuscript Images',
       author='Giancarlo Buomprisco',
-      author_email='giancarlopsk@pgmail.com',
+      author_email='giancarlopsk@gmail.com',
       url='https://github.com/Gbuomprisco/Digital-Lightbox',
-      packages=[],
-     )
+      packages=find_packages(),
+)
